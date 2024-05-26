9e97e5c7f9e7041b5bb39dca34503489166b52b8
Palveet Kaur Saluja <52796065+Palveet@users.noreply.github.com>
2024-05-26 16:52:51 +0530

    242 valid anagram

---
+++ arrays & hashing/242 valid anagram.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())return false;
+        unordered_map<char,int> smap;
+        unordered_map<char,int> tmap;
+        for(int i=0;i<s.size();i++){
+            smap[s[i]]++;
+            tmap[t[i]]++;
+        }
+        for(int i=0;i<smap.size();i++){
+            if(smap[i]!=tmap[i])return false;
+        }
+        return true;
+    }
+};
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         unordered_map<char, int> count;
+        
+//         for (auto x : s) {
+//             count[x]++;
+//         }
+        
+//         for (auto x : t) {
+//             count[x]--;
+//         }
+        
+//         for (auto x : count) {
+//             if (x.second != 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// };
+
+// count solution : 7ms, 9.67mb
+// unordered map solution : 8ms, 10.16mb
