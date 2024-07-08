8a2ebf9e326983bc834f4f5e5da9ef5cf1e02061
Palveet Kaur Saluja <52796065+Palveet@users.noreply.github.com>
2024-07-09 00:46:17 +0530

    evaluate division algo

---
+++ Graphs/399 evaluate division.cpp
@@ -0,0 +1,8 @@
+/* 
+Algo: 
+ a->b->c essentially represents a/b *b/c = a/c 
+ each edge showss a path from numerator to denominator and the weight is the dvision value.
+ to get the reverse invert the weight of each map.
+
+ create and adj list and run dfs/bs 
+*/
