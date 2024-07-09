4345dda5e66374cc57b0745b7705c63471495c50
Palveet Kaur Saluja <52796065+Palveet@users.noreply.github.com>
2024-07-10 00:39:19 +0530

    couting elements

---
+++ arrays & hashing/1426 couting elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countElements(vector<int>& arr) {
+        int ans = 0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        for(const auto& ele:mp){
+            if(mp.find(ele.first+1)!=mp.end()){
+                ans+=ele.second;
+            }
+        }
+        return ans;
+    }
+};
+
+//TC:O(N)
+//SC:O(1)
